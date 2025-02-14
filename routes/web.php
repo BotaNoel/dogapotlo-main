b14267d36649a2351dbab078ac4d893d36f81184
BotaNoel <77171449+BotaNoel@users.noreply.github.com>
2025-02-14 09:09:40 +0100

    kesz

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BooksController;
+use App\Http\Controllers\BorrowingsController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +17,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/books', [BooksController::class, 'index'])->name('books.index');
+    Route::get('/books/{book}', [BooksController::class, 'show'])->name('books.show');
+});
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/borrowings', [BorrowingsController::class, 'index'])->name('borrowings.index');
 });
 
 require __DIR__.'/auth.php';
